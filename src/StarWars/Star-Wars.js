640564ef46447e96643555753b811d300040f5aa
CarolynnFleming <cfcatpink@gmail.com>
2022-01-27 14:20:01 -0500

    starwars array

---
+++ src/StarWars/Star-Wars.js
@@ -0,0 +1,43 @@
+export const starWars = [
+  {
+    side:'Dark Side',
+    totalMembers: 3,
+    img: 'https://pixabay.com/photos/star-wars-darth-vader-dark-darth-1386790/200/200',
+    troopLeader: {
+      name: 'Targin Leeks',
+      age: 25,
+      img: 'https://pixabay.com/photos/star-wars-warrior-space-2473201/200/200'
+    },
+    members: [
+      {
+        name:'Darth Vador',
+        color: 'red',
+      },
+      {
+        name: 'Biggalo Grey',
+        color: 'red'
+      },
+    ]
+
+  },
+  {
+    side: 'Light Side',
+    totalMembers: 3,
+    img: 'https://pixabay.com/vectors/yoda-jedi-star-wars-922520/200/200',
+    troopLeader: {
+      name: 'Luke Skywalker',
+      age: 20,
+      img: 'https://pixabay.com/photos/star-wars-han-solo-luke-skywalker-1724897/200/200'
+    },
+    members: [
+      {
+        name: 'Han Solo',
+        color: 'lime green',
+      },
+      {
+        name: 'Yoda',
+        color: 'light blue'
+      },
+    ]
+  }
+];
