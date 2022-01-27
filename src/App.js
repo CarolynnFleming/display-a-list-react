bd90bf6e8f74e173016b740201d48b302f1e96d2
CarolynnFleming <cfcatpink@gmail.com>
2022-01-27 15:02:14 -0500

    star wars list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import StarWarsList from './StarWarsList/StarWarsList';
+import { starWars } from './StarWarsList/star-wars';
 // import your arrays here
 
 function App() {
   return (
     <div className="App">
         Render all your lists here. Pass the arrays as props.
+      <StarWarsList starWars={starWars} />
     </div>
   );
 }
