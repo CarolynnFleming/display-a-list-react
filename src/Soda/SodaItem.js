f636d5fafd24d06f88572f770fa8dbbd7c4da119
CarolynnFleming <cfcatpink@gmail.com>
2022-01-28 01:47:17 -0500

    progress

--- src/Soda/SodaItem.js
+++ src/Soda/SodaItem.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import styles from './soda.module.css';
 
-export default function SodaItem() {
-  return <div></div>;
+export default function SodaItem(props) {
+  return <div className={styles['soda-item']}>
+    { props.sodaPop }
+  </div>;
 }
 
--- src/Soda/SodaList.js
+++ src/Soda/SodaList.js
@@ -2,7 +2,11 @@ import React from 'react';
 import styles from './soda.module.css';
 import SodaItem from './SodaItem.js';
 
-export default function SodaList() {
-  return <div></div>;
+export default function SodaList(props) {
+  return <div className={styles['soda-list']}>
+    {
+      props.sodaPop.map((soda, i) => <SodaItem key={i}{...soda}/>)
+    }
+  </div>;
 }
 
