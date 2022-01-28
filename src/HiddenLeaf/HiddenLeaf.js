47b36ea37bfcdfe846765d33558f31940ec429a0
CarolynnFleming <cfcatpink@gmail.com>
2022-01-28 01:11:42 -0500

    ninjia

--- src/HiddenLeaf/HiddenLeaf.js
+++ src/HiddenLeaf/HiddenLeaf.js
@@ -1,1 +1,14 @@
-export const hiddenLeaf = ['Naruto', 'Sasuke', 'Sakura', 'Rock Lee'];
+export const hiddenLeaf = [
+  { name: 'Naruto',
+    img : './little-naruto.gif',
+  },
+  { name: 'Sasuke',
+    img: './lil-saske.gif',
+  },
+  { name: 'Sakura',
+    img: './lil-sakura.gif',
+  },
+  { name: 'Rock Lee',
+    img: './lil-rock.gif'
+  }
+];
