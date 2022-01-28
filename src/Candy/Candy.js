f39dae699fd7a337dd59ad6d586f97240f29e269
CarolynnFleming <cfcatpink@gmail.com>
2022-01-27 19:45:56 -0500

    candy

---
+++ src/Candy/Candy.js
@@ -0,0 +1,23 @@
+
+export const candyLand = [
+    {
+        name: 'Jolly Rancher',
+        age: 71,
+        img: './jolly-rancher.jpeg'
+    },
+    {
+        name: 'Tootsie Pop',
+        age: 91,
+        img: './tootsie-pop.jpeg'
+    },
+    {
+        name: 'starburst',
+        age:  63,
+        img: './starburst.png'
+    },
+    {
+        name: 'Mike and Ike',
+        age: 82,
+        img: './mike.png'
+    }
+]
